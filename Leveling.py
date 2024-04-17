251e138e14a6907dbbc2956987390d753e2c2c8b
acosmic <clintmireles@gmail.com>
2024-04-17 13:03:47 -0500

    Update exp calculation and fixed streak bonus in On_Message.py

--- Leveling.py
+++ Leveling.py
@@ -2,10 +2,10 @@ import math
 
 class Leveling:
     def __init__(self):
-        self.base_exp = 25
+        self.base_exp = 125
 
     def calc_level(self, exp):
-        level = math.floor((exp / self.base_exp) ** 0.5)
+        level = math.floor((exp / self.base_exp) ** 0.5) 
         return level
    
     def calc_exp_required(self, level):
