b4cf937af24139aba77e424e94d7db909b7df45e
acosmic <clintmireles@gmail.com>
2024-04-24 22:19:07 -0500

    Added user argument to balance and rank commands

--- Cogs/Balance.py
+++ Cogs/Balance.py
@@ -1,3 +1,4 @@
+from email import message
 import discord
 from discord.ext import commands
 from discord import app_commands
@@ -12,11 +13,18 @@ class Balance(commands.Cog):
         self.bot = bot
 
     @app_commands.command(name="balance", description="Check your Credit balance.")
-    async def balance(self, interaction: discord.Interaction):
-        
+    async def balance(self, interaction: discord.Interaction, user: discord.User = None):
         dao = UserDao()
-        user = dao.get_user(interaction.user.id)
-        await interaction.response.send_message(f"## Your balance: {user.currency:,.0f} Credits. <:PepeRich:1200265584877772840> {interaction.user.mention}")
+
+        if user is None:
+            bot_user = dao.get_user(interaction.user.id)
+            message_text = f"## Your balance: {bot_user.currency:,.0f} Credits. <:PepeRich:1200265584877772840> {interaction.user.mention}" 
+        else:
+            bot_user = dao.get_user(user.id)
+            message_text = f"## {user.name}'s balance: {bot_user.currency:,.0f} Credits. <:PepeRich:1200265584877772840> {interaction.user.mention}" 
+        
+            
+        await interaction.response.send_message(message_text)
         logger.info(f"{interaction.user.name} used /balance command")
 
 async def setup(bot: commands.Bot):
