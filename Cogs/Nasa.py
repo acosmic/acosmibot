cfcc9078bef82938cdb65b0be0113ac6571820a7
acosmic <clintmireles@gmail.com>
2024-04-24 22:19:07 -0500

    Added user argument to balance and rank commands

--- Cogs/Nasa.py
+++ Cogs/Nasa.py
@@ -1,75 +1,3 @@
-# from math import log
-# import discord
-# from discord.ext import commands
-# from discord import app_commands
-# import logging
-# import requests
-# from dotenv import load_dotenv
-# import os
-# from datetime import datetime
-
-# logging.basicConfig(filename='/home/acosmic/Dev/acosmibot/logs.txt', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-# class Nasa(commands.Cog):
-#     def __init__(self, bot:commands.Bot):
-#         super().__init__()
-#         self.bot = bot
-#         load_dotenv()
-#         self.URL = f"https://apod.nasa.gov/apod/"
-        
-
-#     @app_commands.command(name = "apod", description = "Returns the Astronomy Picture of the Day.")
-#     async def apod(self, interaction: discord.Interaction):
-        
-#         logging.info(f"{interaction.user.name} used /nasa - before try block.")
-#         try:
-
-#             # Get today's date
-#             today = datetime.today()
-
-#             # Format the date as YY MM DD
-#             date_formatted = today.strftime("%y%m%d")
-
-#             response = requests.get(self.URL)
-#             data = response.text
-#             # logging.info(data)
-
-#             # Find the start index of the image source
-#             start_index = data.find('IMG SRC="') + len('IMG SRC="')
-#             logging.info(f"start_index: {start_index}")
-
-#             # Find the end index of the image source
-#             end_index = data.find('"', start_index)
-#             logging.info(f"end_index: {end_index}")
-
-#             # Extract the image link
-#             image_link = data[start_index:end_index]
-#             logging.info(f"image_link: {image_link}")
-#             apod_image = f"{self.URL}{image_link}"
-#             logging.info(f"apod_image: {apod_image}")
-
-#             post_url = f"https://apod.nasa.gov/apod/ap{date_formatted}.html"
-#             # Extract the title and explanation
-#             title = data.split('<b>')[1].split('</b>')[0]
-#             # explanation = data.split('<p>')[1].split('</p>')[0]
-#             embed = discord.Embed(title="Astronomy Picture of the Day", color=interaction.user.color)
-#             embed.add_field(name=title, value="", inline=False)
-#             # embed.add_field(name="Explanation", value=explanation, inline=False)
-#             embed.set_image(url=apod_image)
-#             embed.add_field(name="", value=f"[Source: NASA APOD]({post_url}])", inline=False)
-            
-#             await interaction.response.send_message(embed=embed)
-#             logging.info(f"{interaction.user.name} used /nasa")
-#         except Exception as e:
-#             logging.error(f"date_formatted: {date_formatted}")
-#             logging.error(f"today: {today}")
-#             logging.error(f"post_url: {post_url}")
-#             logging.error(f"An error occurred while fetching the Astronomy Picture of the Day: {e}")
-
-# async def setup(bot: commands.Bot):
-#     await bot.add_cog(Nasa(bot))
-
-
 import discord
 from discord.ext import commands
 from discord import app_commands
@@ -112,7 +40,8 @@ class Nasa(commands.Cog):
                 media_type = "an Image"
 
             embed = discord.Embed(title="Astronomy Picture of the Day", description=f"Today's APOD is {media_type}.", color=interaction.user.color)
-            if media_type == "Image":
+            if media_type == "an Image":
+                
                 embed.set_image(url=media_link)
             else:
                 embed.add_field(name="Watch Video", value=f"[Click here to watch the video]({media_link})")
--- Cogs/Rank.py
+++ Cogs/Rank.py
@@ -1,3 +1,4 @@
+from calendar import c
 import discord
 from discord.ext import commands
 from discord import app_commands
@@ -7,16 +8,16 @@ from Entities.User import User
 from logger import AppLogger
 
 
-role_level_1 = "Level One"
-role_level_2 = "Level Two"
-role_level_3 = "Level Three"
-role_level_4 = "Level Four"
-role_level_5 = "Level Five"
-role_level_6 = "Level Six"
-role_level_7 = "Level Seven"
-role_level_8 = "Level Eight"
-role_level_9 = "Level Nine"
-role_level_10 = "Level Ten"
+# role_level_1 = "Level One"
+# role_level_2 = "Level Two"
+# role_level_3 = "Level Three"
+# role_level_4 = "Level Four"
+# role_level_5 = "Level Five"
+# role_level_6 = "Level Six"
+# role_level_7 = "Level Seven"
+# role_level_8 = "Level Eight"
+# role_level_9 = "Level Nine"
+# role_level_10 = "Level Ten"
 
 logging = AppLogger(__name__).get_logger()
 
@@ -26,21 +27,27 @@ class Rank(commands.Cog):
         super().__init__()
         self.bot = bot
 
-    @app_commands.command(name = "rank", description = "Returns your rank based on current EXP and general stats.") 
-    async def rank(self, interaction: discord.Interaction):
+    @app_commands.command(name = "rank", description = "Leave blank to see your own rank, or mention another user to see their rank.") 
+    async def rank(self, interaction: discord.Interaction, user: discord.User = None):
         
         dao = UserDao()
         cfdao = CoinflipDao()
-        user_rank = dao.get_user_rank(interaction.user.id)
-        
-        current_user = dao.get_user(interaction.user.id)
+        if user is not None:
+            user_rank = dao.get_user_rank(user.id)
+            current_user = dao.get_user(user.id)
+            discord_user = user
+        else: 
+            user_rank = dao.get_user_rank(interaction.user.id)
+            current_user = dao.get_user(interaction.user.id)
+            discord_user = interaction.user
 
-        flips = cfdao.get_total_flips(interaction.user.id) if cfdao.get_total_flips(interaction.user.id) is not None else 0
-        flip_wins = cfdao.get_flip_wins(interaction.user.id) if cfdao.get_flip_wins(interaction.user.id) is not None else 0
-        flip_losses = cfdao.get_flip_losses(interaction.user.id) if cfdao.get_flip_losses(interaction.user.id) is not None else 0
 
-        flip_amount_won = cfdao.get_total_won(interaction.user.id) if cfdao.get_total_won(interaction.user.id) is not None else 0
-        flip_amount_lost = cfdao.get_total_lost(interaction.user.id) if cfdao.get_total_lost(interaction.user.id) is not None else 0
+        flips = cfdao.get_total_flips(current_user.id) if cfdao.get_total_flips(current_user.id) is not None else 0
+        flip_wins = cfdao.get_flip_wins(current_user.id) if cfdao.get_flip_wins(current_user.id) is not None else 0
+        flip_losses = cfdao.get_flip_losses(current_user.id) if cfdao.get_flip_losses(current_user.id) is not None else 0
+
+        flip_amount_won = cfdao.get_total_won(current_user.id) if cfdao.get_total_won(current_user.id) is not None else 0
+        flip_amount_lost = cfdao.get_total_lost(current_user.id) if cfdao.get_total_lost(current_user.id) is not None else 0
 
         flip_win_rate = flip_wins / flips * 100 if flips > 0 else 0
 
@@ -49,7 +56,7 @@ class Rank(commands.Cog):
 
         if user_rank is not None:
             name_from_db = user_rank[1]
-            display_name = interaction.user.name if interaction.user.name is not None else name_from_db
+            display_name = discord_user.name if discord_user.name is not None else name_from_db
             level_emoji = "🥚"
             if current_user.level >= 5:
                 level_emoji = "🐣"
@@ -59,7 +66,7 @@ class Rank(commands.Cog):
                 level_emoji = "🐓"
             
             embed = discord.Embed(
-            # title=f"### {interaction.user.name}",
+            # title=f"### {discord_user.name}",
             description=(
             f"# {display_name} {level_emoji}\n\n"
             f"### Ranked #{user_rank[-1]}\n"
@@ -75,44 +82,45 @@ class Rank(commands.Cog):
             f"Coinflip Win Rate: {flip_win_rate:.2f}%\n\n"
             f"### Streak: {streak_emoji}\n"
             ),
-            color=interaction.user.color)
-            embed.set_thumbnail(url=interaction.user.avatar)
+            color=discord_user.color)
+            embed.set_thumbnail(url=discord_user.avatar)
 
             await interaction.response.send_message(embed=embed)
 
         else:
-            logging.info(f"The user with Discord username {interaction.user.name} was not found in the database.")
+        #     logging.info(f"The user with Discord username {interaction.user.name} was not found in the database.")
             
-            role = discord.utils.get(interaction.user.guild.roles, name=role_level_1)
-            await interaction.user.add_roles(role)
-            join_date = interaction.user.joined_at
+        #     role = discord.utils.get(interaction.user.guild.roles, name=role_level_1)
+        #     await interaction.user.add_roles(role)
+        #     join_date = interaction.user.joined_at
 
-            # Convert join_date to a format suitable for database insertion (e.g., as a string)
-            formatted_join_date = join_date.strftime("%Y-%m-%d %H:%M:%S")
+        #     # Convert join_date to a format suitable for database insertion (e.g., as a string)
+        #     formatted_join_date = join_date.strftime("%Y-%m-%d %H:%M:%S")
 
-            user_data = {
-            'id': interaction.user.id,
-            'discord_username': str(interaction.user.name),
-            'level': 1,
-            'streak': 0,
-            'exp': 0,
-            'exp_gained': 0,
-            'exp_lost': 0,
-            'currency': 0,
-            'messages_sent': 1,
-            'reactions_sent': 0,
-            'created': formatted_join_date,
-            'last_active': formatted_join_date,
-            'daily': 0,
-            'last_daily': None,
-            }
+        #     user_data = {
+        #     'id': interaction.user.id,
+        #     'discord_username': str(interaction.user.name),
+        #     'level': 1,
+        #     'streak': 0,
+        #     'exp': 0,
+        #     'exp_gained': 0,
+        #     'exp_lost': 0,
+        #     'currency': 0,
+        #     'messages_sent': 1,
+        #     'reactions_sent': 0,
+        #     'created': formatted_join_date,
+        #     'last_active': formatted_join_date,
+        #     'daily': 0,
+        #     'last_daily': None,
+        #     }
 
-            new_user = User(**user_data)
-            dao.add_user(new_user)
-            logging.info(f'{new_user.discord_username} added to the database.')
-            await interaction.response.send_message(f'{interaction.user.name} was not found in the database. {new_user.discord_username} added to the database.')
-        # await interaction.response.send_message("Hello!") 
-        logging.info(f"{interaction.user.name} used /rank command")
+        #     new_user = User(**user_data)
+        #     dao.add_user(new_user)
+        #     logging.info(f'{new_user.discord_username} added to the database.')
+        #     await interaction.response.send_message(f'{interaction.user.name} was not found in the database. {new_user.discord_username} added to the database.')
+        # # await interaction.response.send_message("Hello!") 
+        # logging.info(f"{interaction.user.name} used /rank command")
+            return
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Rank(bot))
