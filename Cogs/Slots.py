332576813da3132b2896374470540395392e6b8d
acosmic <clintmireles@gmail.com>
2024-06-06 20:32:30 -0500

    Refactor setup hook in main.py fixed duplicate extensions and tasks

--- Cogs/Slots.py
+++ Cogs/Slots.py
@@ -35,6 +35,11 @@ class Slots(commands.Cog):
         slot2 = random.choice(self.slots)
         slot3 = random.choice(self.slots)
 
+        # if interaction.user.id == 110637665128325120:
+        #     slot1 = "<:acosmicD:1171219346299814009>"
+        #     slot2 = "<:acosmicD:1171219346299814009>"
+        #     slot3 = "<:acosmicD:1171219346299814009>"
+
         result = f"| {slot1} | {slot2} | {slot3} |\n"
         embed = discord.Embed()
         if slot1 == slot2 == slot3 == "<:acosmicD:1171219346299814009>":
@@ -44,28 +49,28 @@ class Slots(commands.Cog):
             user.season_exp += 1000
             amount_won = cost * 100
             amount_lost = 0
-            embed.title = f"<:acosmicFaded:1236023522921414707> {interaction.user.name}'s Slot Machine Result <:acosmicFaded:1236023522921414707>"
-            embed.description = f"# {result}\nSUPER JACKPOT! You won {amount_won:,.0f} credits and 1,000 EXP! <a:peepoRich:855180219772960798>"
+            # embed.title = f"<:acosmicFaded:1236023522921414707> {interaction.user.name}'s Slot Machine Result <:acosmicFaded:1236023522921414707>"
+            embed.description = f"# <a:OOOOM:1236019284904509621> SUPER JACKPOT <a:OOOOM:1236019284904509621>\n\n\n # {result}\n{interaction.user.mention} hit the SUPER Jackpot and won {amount_won:,.0f} credits!"
             embed.color = discord.Color.gold()
             general_embed = discord.Embed()
-            general_embed.title = f"🎰 {interaction.user.name} hit the SUPER Jackpot! 🎰"
-            general_embed.description = f"# {result}\n{interaction.user.mention} hit the SUPER Jackpot and won {amount_won:,.0f} credits! <a:peepoRich:855180219772960798>"
+            # general_embed.title = f"🎰 {interaction.user.name} hit the SUPER Jackpot! 🎰"
+            general_embed.description = f"# <a:OOOOM:1236019284904509621> SUPER JACKPOT <a:OOOOM:1236019284904509621>\n\n\n # {result}\n{interaction.user.mention} hit the SUPER Jackpot and won {amount_won:,.0f} credits!"
             general_embed.color = discord.Color.gold()
             general_embed.set_footer(text="Try your luck with /slots! in 🎰︱casino")
             await general_channel.send(embed=general_embed)
-        if slot1 == slot2 == slot3 == "🦄":
+        elif slot1 == slot2 == slot3 == "🦄":
             user.currency += cost * 50  # Mega Jackpot
             amount_won = cost * 50
             user.exp += 500
             user.exp_gained += 500
             user.season_exp += 500
             amount_lost = 0
-            embed.title = f"<a:OOOOM:1236019284904509621> {interaction.user.name}'s Slot Machine Result <a:OOOOM:1236019284904509621>"
-            embed.description = f"# {result}\nMEGA JACKPOT! You won {amount_won:,.0f} credits and 500 EXP! <a:peepoRich:855180219772960798>"
+            # embed.title = f"<a:OOOOM:1236019284904509621> {interaction.user.name}'s Slot Machine Result <a:OOOOM:1236019284904509621>"
+            embed.description = f"# <a:OOOOM:1236019284904509621> {interaction.user.name} <a:OOOOM:1236019284904509621>\n\n\n # {result}\nMEGA JACKPOT! You won {amount_won:,.0f} credits and 500 EXP!"
             embed.color = discord.Color.gold()
             general_embed = discord.Embed()
-            general_embed.title = f"🎰 {interaction.user.name} hit the MEGA Jackpot! 🎰"
-            general_embed.description = f"# {result}\n{interaction.user.mention} hit the MEGA Jackpot and won {amount_won:,.0f} credits! <a:peepoRich:855180219772960798>"
+            # general_embed.title = f"🎰 {interaction.user.name} hit the MEGA Jackpot! 🎰"
+            general_embed.description = f"# <a:OOOOM:1236019284904509621> {interaction.user.name} <a:OOOOM:1236019284904509621>\n\n\n # {result}\n{interaction.user.mention} hit the MEGA Jackpot and won {amount_won:,.0f} credits!"
             general_embed.color = discord.Color.gold()
             general_embed.set_footer(text="Try your luck with /slots! in 🎰︱casino")
             await general_channel.send(embed=general_embed)
@@ -76,12 +81,12 @@ class Slots(commands.Cog):
             user.exp_gained += 250
             user.season_exp += 250
             amount_lost = 0
-            embed.title = f"<a:OOOOM:1236019284904509621> {interaction.user.name}'s Slot Machine Result <a:OOOOM:1236019284904509621>"
-            embed.description = f"# {result}\nJackpot! You won {amount_won:,.0f} credits and 250 EXP! 🤑"
+            # embed.title = f"<a:OOOOM:1236019284904509621> {interaction.user.name}'s Slot Machine Result <a:OOOOM:1236019284904509621>"
+            embed.description = f"# <a:OOOOM:1236019284904509621> {interaction.user.name} <a:OOOOM:1236019284904509621>\n\n\n # {result}\nJackpot! You won {amount_won:,.0f} credits and 250 EXP! 🤑"
             embed.color = discord.Color.gold()
             general_embed = discord.Embed()
-            general_embed.title = f"🎰 {interaction.user.name} hit the Jackpot! 🎰"
-            general_embed.description = f"# {result}\n{interaction.user.mention} hit the Jackpot and won {amount_won:,.0f} credits! 🤑"
+            # general_embed.title = f"🎰 {interaction.user.name} hit the Jackpot! 🎰"
+            general_embed.description = f"# <a:peepoGamba:1247551104414257262> {interaction.user.name} <a:peepoGamba:1247551104414257262>\n\n\n # {result}\n{interaction.user.mention} hit the Jackpot and won {amount_won:,.0f} credits! 🤑"
             general_embed.color = discord.Color.gold()
             general_embed.set_footer(text="Try your luck with /slots! in 🎰︱casino")
             await general_channel.send(embed=general_embed)
@@ -92,13 +97,13 @@ class Slots(commands.Cog):
             user.exp_gained += 20
             user.season_exp += 20
             amount_lost = 0
-            embed.title = f"⭐ {interaction.user.name}'s Slot Machine Result ⭐"
-            embed.description = f"# {result}\nYou matched two! You won {amount_won:,.0f} credits and 20 EXP! 🥳"
+            # embed.title = f"⭐ {interaction.user.name}'s Slot Machine Result ⭐"
+            embed.description = f"# <a:peepoGamba:1247551104414257262> {interaction.user.name} <a:peepoGamba:1247551104414257262>\n\n\n # {result}\nYou matched two! You won {amount_won:,.0f} credits and 20 EXP! 🥳"
             embed.color = discord.Color.green()
         else:
             vdao = VaultDao()
             vcredits = vdao.get_currency()
-            vgain = cost * 0.01 # 1% of the lost credits
+            vgain = cost * 0.05 # 5% of the bet
             vcredits += vgain
             vdao.update_currency(vcredits)
             user.currency -= cost  # Loss
@@ -107,8 +112,8 @@ class Slots(commands.Cog):
             user.exp_gained += 2
             user.season_exp += 2
             amount_lost = cost
-            embed.title = f"😢 {interaction.user.name}'s Slot Machine Result 😢"
-            embed.description = f"# {result}\nNo match, better luck next time! You lost {amount_lost:,.0f} credits, but gained 2 EXP.\n\n{vgain:,.0f} Credits have been added to the vault! 🏦"
+            # embed.title = f"<a:peepoGamba:1247551104414257262> {interaction.user.name} <a:peepoGamba:1247551104414257262>"
+            embed.description = f"# <a:peepoGamba:1247551104414257262> {interaction.user.name} <a:peepoGamba:1247551104414257262>\n\n\n # {result}\nYou lost {amount_lost:,.0f} credits, but gained 2 EXP.\n\n{vgain:,.0f} Credits have been added to the vault! 🏦"
             embed.color = discord.Color.red()
 
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
